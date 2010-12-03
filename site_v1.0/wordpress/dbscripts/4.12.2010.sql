66d089b89bb7ee8f56df9c40fcf55715383b2f7e
doruk.altan <doruk.altan@00ff942a-6c99-8e64-ae53-d19d570b70e4>
2010-12-03 22:54:18 +0000

    dbscripts klasörüne wp_comment_arti_eksi tablosunun ddl'i eklendi.

---
+++ site_v1.0/wordpress/dbscripts/4.12.2010.sql
@@ -0,0 +1,32 @@
+-- phpMyAdmin SQL Dump
+-- version 3.3.7deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Dec 04, 2010 at 12:50 AM
+-- Server version: 5.1.49
+-- PHP Version: 5.3.3-1ubuntu9.1
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `wordpress`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_comment_arti_eksi`
+--
+
+CREATE TABLE IF NOT EXISTS `wp_comment_arti_eksi` (
+  `comment_ID` bigint(20) unsigned NOT NULL,
+  `comment_arti_eksi` varchar(1) NOT NULL,
+  PRIMARY KEY (`comment_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
