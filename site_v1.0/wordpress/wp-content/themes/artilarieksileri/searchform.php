468e4cdf22c695046214bf2eaaf5913abc111130
dorukaltan <dorukaltan@00ff942a-6c99-8e64-ae53-d19d570b70e4>
2011-01-15 20:01:24 +0000

    arama çubuğu yazısı türkçeleştirildi.

--- site_v1.0/wordpress/wp-content/themes/artilarieksileri/searchform.php
+++ site_v1.0/wordpress/wp-content/themes/artilarieksileri/searchform.php
@@ -1,7 +1,7 @@
 <div id="searchForm">
 	<form action="<?php bloginfo('url'); ?>" method="get" accept-charset="utf-8">
 		<fieldset>
-			<input type="text" name="s" id="s" value="<?php the_search_query(); ?>" class="hint" title="Search..." />
+			<input type="text" name="s" id="s" value="<?php the_search_query(); ?>" class="hint" title="Arama..." />
 			<button type="submit" name="submitButton" id="submitButton" title="Search"><span><em>Search</em></span></button>
 		</fieldset>
 	</form>
