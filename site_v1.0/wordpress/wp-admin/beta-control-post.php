691fcaeb35dec056daa089b4c5c588e69711bd8e
dorukaltan <dorukaltan@00ff942a-6c99-8e64-ae53-d19d570b70e4>
2011-02-19 16:05:54 +0000

    update on beta files

--- site_v1.0/wordpress/wp-admin/beta-control-post.php
+++ site_v1.0/wordpress/wp-admin/beta-control-post.php
@@ -1,47 +1,64 @@
 <?php
-  require( dirname(__FILE__) . '/../wp-load.php' );
-  nocache_headers();  
+require( dirname(__FILE__) . '/../wp-load.php' );
+nocache_headers();
 
-  $debug=true;
-
-  foreach (array_keys($_POST) as $degis)
-  {
+foreach (array_keys($_POST) as $degis)
+{
     $komut=$_POST[$degis];
     $degis = explode("_", $degis);
     $betano = $degis[count($degis)-1];
-if($debug)print $komut;
+
     if($komut=="kaydet")
     {
-//elle veritabanına minimum değerler vererek kayıt yap
-/*
-    $wp_hasher = new PasswordHash(8, TRUE);
-    $password = $wp_hasher->HashPassword($password);
-    if(is_numeric($user_id)) {
-        $sql = "UPDATE wp_users SET user_pass = '$password' WHERE ID = $user_id LIMIT 1;";
-        # then connect to the DB and execute mysql_query($sql)
-*/
-      $query = "UPDATE wp_beta set durum='kaydedildi' WHERE betano = $betano;";
-      mysql_query($query);
-if($debug) print $query;
+	$query = "SELECT * FROM wp_beta WHERE betano = $betano;";
+	$result = mysql_query($query);
 
-      $query = "SELECT * FROM wp_beta WHERE betano=$betano;";
-      mysql_query($query);
-if($debug) print $query;
-      $result = mysql_query($query);
-      $to      = $result[eposta];
-      $subject = 'ArtılarıEksileri.co Beta üyeliği isteği';
-      $message = 'Merhaba ArtılarıEksileri.co Beta üyeliğine kabul edildiniz.';
-      $headers = 'From: bilgi@artilarieksileri.co' . "\r\n" .
-	  'X-Mailer: PHP/' . phpversion();
-      mail($to, $subject, $message, $headers);
+	if($row = mysql_fetch_assoc($result))
+	{	
+	    $user_name = $row[kullaniciadi];
+	    $user_mail = $row[eposta];
+	    require_once(ABSPATH . WPINC . '/registration.php');
+
+	    $user_id = username_exists( $user_name );
+	    $random_password;
+	    if ( !$user_id ) 
+	    {
+		$random_password = wp_generate_password( 12, false );
+		$user_id = wp_create_user( $user_name, $random_password, $user_email );
+	    } 
+	    else 
+	    {
+		$random_password = __('User already exists.  Password inherited.');
+	    }
+	    
+    //wp_beta tablosunu güncelle
+	    if(is_numeric ($user_id))
+	    {
+		$query = "UPDATE wp_beta set durum='kaydedildi' WHERE betano = $betano;";
+		mysql_query($query);
+	    }
+    //wp_beta tablosunu güncelle
+
+    /*E-POSTA GONDERME
+	$query = "SELECT * FROM wp_beta WHERE betano=$betano;";
+	mysql_query($query);
+    if($debug) print $query;
+	$result = mysql_query($query);
+	$to      = $result[eposta];
+	$subject = 'ArtılarıEksileri.co Beta üyeliği isteği';
+	$message = 'Merhaba ArtılarıEksileri.co Beta üyeliğine kabul edildiniz.';
+	$headers = 'From: bilgi@artilarieksileri.co' . "\r\n" .
+	    'X-Mailer: PHP/' . phpversion();
+	mail($to, $subject, $message, $headers);
+    */
+	}
     }
     else if($komut=="sil")
     {
-      
+	$query = "DELETE FROM wp_beta WHERE betano = $betano;";
+	mysql_query($query);
     }
+print "$komut işlemi sorunsuz tamamlandı.Yönlendiriliyorsunuz.";
 
-  }
-  return;
-  $query = "SELECT * FROM wp_beta WHERE durum = 'beklemede';";
-  $result = mysql_query($query);
+}
 ?>
--- site_v1.0/wordpress/wp-beta-post.php
+++ site_v1.0/wordpress/wp-beta-post.php
@@ -4,19 +4,29 @@ require( dirname(__FILE__) . '/wp-load.php' );
 nocache_headers();
 
 $eposta = is_email(isset($_POST['email']) ? $_POST['email'] : "");
-if (!$eposta)
+$kullaniciadi = isset($_POST['username']) ? $_POST['username'] : "";
+if(!$kullaniciadi)
 {
-  print "eposta geçersiz";
+  print "kullanıcı adı boş olmamalı.";
+  return;
+}
+else if (!$eposta)
+{
+  print "eposta geçersiz.";
   return;
 }
 $ipadres = $_SERVER['REMOTE_ADDR'];
 $tarih = date("Y-n-j");
+$durum = 'beklemede';
+$data = compact('kullaniciadi', 'eposta', 'ipadres', 'tarih', 'durum');
+//print_r($data);
+$cikti = $wpdb->insert($wpdb->beta, $data);
 
-$data = compact('eposta', 'ipadres', 'tarih');
-
-if ($wpdb->insert($wpdb->beta, $data) == 1)
-  print "Beta kaydınız tamamlanmıştır. Bizden haber bekleyin. Bir kaç gün içinde size döneceğiz";
+if ($cikti == 1)
+  print "<font color='green'>Beta kaydınız tamamlanmıştır. Bizden haber bekleyin. Bir kaç gün içinde size döneceğiz.</font>";
 else
-  print "hata";
+{
+  print "<font color='red'>Kaydınız tamamlanmadı.</font>";
+}
 
 ?>
